3d685628690b65aa7e43db98b5c078847e183566
Аким Фасхутдинов <a.faskhutdinov@yclients.tech>
2021-01-06 10:15:51 +0300

    fixed version

--- yandex_cloud_client/version.py
+++ yandex_cloud_client/version.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 __url__ = "https://github.com/akimrx/python-yc-client"
 __description__ = "PUnofficial Yandex.Cloud REST API Client"
 __author__ = "Akim Faskhutdinov"
